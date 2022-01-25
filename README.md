978ff8eb460c37862ba25287c55d2ad5ce5e46de
one11a <92191015+one11a@users.noreply.github.com>
2022-01-25 18:20:09 +1100

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# cannon-start
+# PRO-C22-SA-Boilerplate_code
+Boilerplate code for student  
---
+++ js/Cannon.js
@@ -0,0 +1,22 @@
+class Cannon {
+  constructor(x, y, width, height, angle) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.angle = angle;
+    this.cannon_image=loadImage("assets/canon.png")
+    this.cannonbase_image=loadImage("assets/cannonBase.png")
+
+  }
+  
+  display(){
+  push()
+  translate(this.x,this.y)
+rotate(this.angle)
+  imageMode(CENTER)
+  image(this.cannon_image,0,0,this.width,this.height)
+  pop()
+  image(this.cannonbase_image,70,20,200,200)  
+  }
+}
